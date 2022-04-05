a565442669297b2d78f0ff831c90509195620088
Bhavana <bhavananerlakota@gmail.com>
2022-04-06 03:02:02 +0530

    Added UC1 Fibonacci series

--- Day6 Assignment/Program.cs
+++ Day6 Assignment/Program.cs
@@ -7,6 +7,19 @@ namespace Day6_Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical programming");
+            int num1 = 0, num2 = 1, temp, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                temp = num1 + num2;
+                Console.Write(temp + " ");
+                num1 = num2;
+                num2 = temp;
+            }
         }
     }
 }
+    
+
