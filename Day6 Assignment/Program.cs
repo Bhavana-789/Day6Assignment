4d741aea3e73736e5ae57b2a953889fb23dd8083
Bhavana <bhavananerlakota@gmail.com>
2022-04-06 03:58:41 +0530

    Added UC5 Coupon number

--- Day6 Assignment/Program.cs
+++ Day6 Assignment/Program.cs
@@ -7,22 +7,28 @@ namespace Day6_Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical programming");
-            //UC4_PrimeNumber
-            Console.WriteLine("Welcome to reverse number Program");
-            Console.WriteLine("Enter a number");
+            //UC5_couponNumber
+            Console.WriteLine("Welcome to coupon number Program");
+            Console.WriteLine("Please Enter how many Random distinct number you want: ");
             int num = int.Parse(Console.ReadLine());
-            int temp, rem, rev = 0;
-            temp = num;
-            while (num > 0)
+            int[] ran = new int[num];
+            Console.WriteLine("Enter the Starting range of Random Number you want");
+            int str = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the ending range of Random numnber you want ");
+            int end = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
             {
-                rem = num % 10;
-                rev = ((rev * 10) + rem);
-                num = num / 10;
+                Random random = new Random();
+                int n = random.Next(str, end);
+                ran[i] = n;
             }
-
-            Console.WriteLine("Reverse of the number " + temp + " is: " + rev);
-
+            Console.WriteLine("All Random Distinct Numbers are: ");
+            foreach (int j in ran)
+            {
+                Console.WriteLine(j);
+            }
         }
+
     }
 }
 
